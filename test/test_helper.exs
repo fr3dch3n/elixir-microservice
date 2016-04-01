00660f38363df40110e3ff4d2f392ae9869f14f9
Frederik Mars <frederik.mars@otto.de>
2016-04-01 23:05:00 +0200

    cleaning up

--- test/test_helper.exs
+++ test/test_helper.exs
@@ -1,6 +1,1 @@
 ExUnit.start
-
-Mix.Task.run "ecto.create", ~w(-r MagellanMicroservice.Repo --quiet)
-Mix.Task.run "ecto.migrate", ~w(-r MagellanMicroservice.Repo --quiet)
-Ecto.Adapters.SQL.begin_test_transaction(MagellanMicroservice.Repo)
-
