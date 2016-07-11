59ce67c20205457d5147c64097b7c2b47fac8e7f
Frederik Mars <frederik.mars@otto.de>
2016-07-11 19:43:36 +0200

    moved files + bugfix in test

--- test/router_test.exs
+++ test/router_test.exs
@@ -1,24 +1,25 @@
-defmodule MagellanMicroservice.Server.Router.Test do
+defmodule MagellanMicroservice.Router.Test do
   use ExUnit.Case, async: true
+  alias MagellanMicroservice.Router
   use Plug.Test
 
-  @opts MagellanMicroservice.Server.Router.init([])
+  @opts MagellanMicroservice.Router.init([])
   test "returns the health code" do
-    conn = MagellanMicroservice.Server.Router.call(conn(:get, "/health"), @opts)
+    conn = Router.call(conn(:get, "/health"), @opts)
     assert conn.state == :sent
     assert conn.status == 200
     assert conn.resp_body == "OK"
   end
 
   test "return the status page" do
-    conn = MagellanMicroservice.Server.Router.call(conn(:get, "/status"), @opts)
+    conn = Router.call(conn(:get, "/status"), @opts)
     assert conn.state == :sent
     assert conn.status == 200
     assert conn.resp_body == "{\"statusDetails\":{\"server\":{\"status\":\"ok\"},\"router\":{\"status\":\"ok\"}},\"status\":\"ok\"}"
   end
 
   test "no matching route should return 404" do
-    conn = MagellanMicroservice.Server.Router.call(conn(:get, "/foobar"), @opts)
+    conn = Router.call(conn(:get, "/foobar"), @opts)
     assert conn.state == :sent
     assert conn.status == 404
   end
