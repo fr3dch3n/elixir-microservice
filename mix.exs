738473b552f87d050579ac38ee5b3486d8f80a30
Frederik Mars <frederik.mars@otto.de>
2016-07-21 09:11:34 +0200

    version 0.1.3

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule MagellanMicroservice.Mixfile do
 
   def project do
     [app: :magellan_microservice,
-     version: "0.1.2",
+     version: "0.1.3",
      elixir: "~> 1.2",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
