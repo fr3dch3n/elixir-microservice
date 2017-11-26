f9de5ef90961de7b91e279cd07f1026f5d9959ad
Frederik Mars <frederik.mars@otto.de>
2017-11-26 18:33:05 +0100

    add basic test for metrics-endpoint

--- test/metrics/prometheus_test.exs
+++ test/metrics/prometheus_test.exs
@@ -1,5 +1,14 @@
-defmodule Prometheus.Test do
+defmodule MagellanMicroservice.PipelineInstrumenter.Test do
   use ExUnit.Case, async: true
+  alias MagellanMicroservice.Router
+  use Plug.Test
 
-  # test metrics endpoint
+  @opts MagellanMicroservice.Router.init([])
+  test "metrics contains the successful request to health" do
+    Router.call(conn(:get, "/test/health"), @opts)
+    conn = Router.call(conn(:get, "/metrics"), @opts)
+    assert conn.state == :sent
+    assert conn.status == 200
+    assert String.contains?(conn.resp_body, "http_requests_total{status_class=\"success\",method=\"GET\",host=\"www.example.com\",scheme=\"http\",request_path=\"/test/health\"}") == :true
+  end
 end
